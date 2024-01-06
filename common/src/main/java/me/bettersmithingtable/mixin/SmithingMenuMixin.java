27a7387c0b1b683bd6557894060a3f624def9284
jjblock21 <jjblock21@gmail.com>
2024-01-06 18:24:45 +0100

    Experimental fix for BetterRecipeBooks

--- common/src/main/java/me/bettersmithingtable/mixin/SmithingMenuMixin.java
+++ common/src/main/java/me/bettersmithingtable/mixin/SmithingMenuMixin.java
@@ -2,15 +2,23 @@ package me.bettersmithingtable.mixin;
 
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.*;
+import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.SmithingRecipe;
 import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Mixin(SmithingMenu.class)
 public abstract class SmithingMenuMixin extends ItemCombinerMenu {
+
+    @Shadow @Final
+    private List<RecipeHolder<SmithingRecipe>> recipes;
 
     public SmithingMenuMixin(@Nullable MenuType<?> menuType, int i, Inventory inventory, ContainerLevelAccess containerLevelAccess) {
         super(menuType, i, inventory, containerLevelAccess);
@@ -21,18 +29,16 @@ public abstract class SmithingMenuMixin extends ItemCombinerMenu {
      */
     @Inject(method = "createInputSlotDefinitions", at = @At("HEAD"), cancellable = true)
     public void createInputSlotDefinitions(CallbackInfoReturnable<ItemCombinerMenuSlotDefinition> cir) {
-        SmithingMenuAccessor accessor = (SmithingMenuAccessor)this;
-
         ItemCombinerMenuSlotDefinition definition = ItemCombinerMenuSlotDefinition.create()
-                .withSlot(0, 64, 35, (stack -> accessor.getRecipes().stream().anyMatch((holder) ->
+                .withSlot(0, 64, 35, (stack -> recipes.stream().anyMatch((holder) ->
                         holder.value().isTemplateIngredient(stack)
                 )))
                 // Armor piece
-                .withSlot(1, 38, 45, (stack -> accessor.getRecipes().stream().anyMatch((holder) ->
+                .withSlot(1, 38, 45, (stack -> recipes.stream().anyMatch((holder) ->
                         holder.value().isBaseIngredient(stack)
                 )))
                 // Trim/Upgrade material
-                .withSlot(2, 18, 25, (stack -> accessor.getRecipes().stream().anyMatch((holder) ->
+                .withSlot(2, 18, 25, (stack -> recipes.stream().anyMatch((holder) ->
                         holder.value().isAdditionIngredient(stack)
                 )))
                 .withResultSlot(3, 142, 35).build();
