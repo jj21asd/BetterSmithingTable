f174bd51a8f4afb10d4ca7a01dd4f770617435eb
jj21 <jjblock21@gmail.com>
2025-08-26 01:14:13 +0200

    fix accidental hard dependency on ModMenu

---
+++ fabric/src/main/java/me/bettersmithingtable/fabric/ModMenuApiImpl.java
@@ -0,0 +1,13 @@
+package me.bettersmithingtable.fabric;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import eu.midnightdust.lib.config.MidnightConfig;
+import me.bettersmithingtable.BetterSmithingTable;
+
+public class ModMenuApiImpl implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> MidnightConfig.getScreen(parent, BetterSmithingTable.MOD_ID);
+    }
+}
