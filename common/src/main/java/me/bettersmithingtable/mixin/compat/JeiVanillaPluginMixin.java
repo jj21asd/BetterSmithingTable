55638ef07b27ab13c0c04dba401645ada97ba924
jj21asd <jadi.er@unity-mail.de>
2026-09-14 21:39:16 +0200

    small code cleanup

--- common/src/main/java/me/bettersmithingtable/mixin/compat/JeiVanillaPluginMixin.java
+++ common/src/main/java/me/bettersmithingtable/mixin/compat/JeiVanillaPluginMixin.java
@@ -10,9 +10,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 // Fixes issue #13
-// I don't know if there's an easier way to do this, a JeiPlugin can't remove click areas sadly
-// Pseudo will spam the console a bit when JEI is not installed,
-// but I can't be bothered to write a mixin config plugin
+// I don't know if there's an easier way to do this, a JeiPlugin can't remove click areas sadly.
+// Pseudo will spam the console a bit when JEI is not installed, but I can't be bothered to
+// write a mixin config plugin.
 @Pseudo
 @Mixin(targets = "mezz.jei.library.plugins.vanilla.VanillaPlugin")
 public class JeiVanillaPluginMixin {
@@ -24,9 +24,8 @@ public class JeiVanillaPluginMixin {
         ),
         remap = false
     )
-    private <T extends HandledScreen<?>> void redirectSmithingClickArea(
-        IGuiHandlerRegistration instance, Class<? extends T> clazz, int x, int y, int width,
-        int height,  RecipeType<?>[] recipeTypes) {
+    private void redirectSmithingClickArea(IGuiHandlerRegistration instance, Class<? extends HandledScreen<?>> clazz,
+                                           int x, int y, int width, int height,  RecipeType<?>[] recipeTypes) {
         if (clazz != SmithingScreen.class) {
             // Register normally
             instance.addRecipeClickArea(clazz, x, y, width, height, recipeTypes);
